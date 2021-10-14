38302146e6dab7810ab7026ff19f5a88ed82a92d
Ledenyinandi <ledenyinandi@gmail.com>
2021-10-14 17:52:07 +0200

    Add sample sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into customer (last_name, first_name, country, city, address, zip, phone_number, email)
+values ('Kovács', 'Zoltán', 'Hungary', 'Szeged', 'Ifjúság 14', '7000', '+36701234567', 'kovacszoltan@gmail.com');
+
+insert into account (account_number, account_type, account_status, balance, begin_balance, begin_time, customer_id)
+values ('12345678-98765432-00000000', 'CHECKING', 'ACTIVE', 10000.0, 10000.0, current_timestamp, 1);
+
+insert into account (account_number, account_type, account_status, balance, begin_balance, begin_time, customer_id)
+values ('12345678-98765432-23459876', 'CHECKING', 'ACTIVE', 12000.0, 12000.0, current_timestamp, 1);
+
+-- insert into account_customers (accounts_id, customers_id) values (1, 1);
+-- insert into account_customers (accounts_id, customers_id) values (2, 1);
