fd1d4dd1da4f83d34bb2db6d4b18f6df6e2b142d
Ledenyinandi <ledenyinandi@gmail.com>
2021-10-14 17:49:25 +0200

    Update repository interfaces

--- src/main/java/nandor/ledenyi/bankapp/repository/CustomerRepository.java
+++ src/main/java/nandor/ledenyi/bankapp/repository/CustomerRepository.java
@@ -3,10 +3,5 @@ package nandor.ledenyi.bankapp.repository;
 import nandor.ledenyi.bankapp.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-    List<Customer> findCustomersByAccounts(Long Id);
-    List<Customer> findCustomersByLastName(String lastName);
 }
