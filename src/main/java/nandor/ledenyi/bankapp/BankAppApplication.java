2bc44f8fcd8af3059a4293e5311600d1d112092a
Ledenyinandi <ledenyinandi@gmail.com>
2021-10-14 19:01:12 +0200

    Set swagger

--- src/main/java/nandor/ledenyi/bankapp/BankAppApplication.java
+++ src/main/java/nandor/ledenyi/bankapp/BankAppApplication.java
@@ -2,6 +2,11 @@ package nandor.ledenyi.bankapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class BankAppApplication {
@@ -10,4 +15,13 @@ public class BankAppApplication {
 		SpringApplication.run(BankAppApplication.class, args);
 	}
 
+	@Bean
+	public Docket swagger() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(this.getClass().getPackageName()))
+				.paths(PathSelectors.any())
+				.build()
+				.useDefaultResponseMessages(false);
+	}
 }
